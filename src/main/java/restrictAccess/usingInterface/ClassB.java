1e1eb12a27845191149693d6a157364e098108cb
abha <abha@example.com>
2019-11-17 20:05:24 +0530

    <<Minor Change>>: updated comments in the code

--- src/main/java/restrictAccess/usingInterface/ClassB.java
+++ src/main/java/restrictAccess/usingInterface/ClassB.java
@@ -14,12 +14,12 @@ public class ClassB implements InterfaceA {
 		
 		/* The target is to allow user to access both the methods <<show() and display()>> */
 		ClassB objB = new ClassB();
-		objB.show();
-		objB.display();
+		objB.show(); //Hello
+		objB.display(); //Hi
 	
 		/* The target is to restrict user form accessing <<display()>> method */
 		InterfaceA objA = new ClassB();
-		objA.show();
+		objA.show(); //Hello
 	  //objA.display(); Error encountered saying, "The method display() is undefined for the type InterfaceA"
 	}
 
